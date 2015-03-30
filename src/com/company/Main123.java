bc09e5003622dd8d16511571f6bdb4575604b58e
Ann <sherilwolf@gmail.com>
2015-03-30 15:10:43 +0300

    home add

---
+++ src/com/company/Main123.java
@@ -0,0 +1,78 @@
+package com.company;
+
+/**
+ * Created by Ann on 27.03.2015.
+ */
+public class Main123 {
+    public static void main(String[] args){
+// Зеленым цветом выделен вывод в консоль в результате выполнения кода.
+        int n = 10;
+        System.out.println(n);
+// 10
+        int m = 20;
+        System.out.println(n + m);
+// 30
+        String v = "Hello class";
+        System.out.println(v);
+// Hello class
+        String w = "15";
+        System.out.println(w);
+// 15
+        System.out.println(v + w);
+// Hello class15
+        if (n < m) {
+            System.out.println("n less then m");
+        }
+// n less then m
+        if (n > m) {
+            System.out.println("n more then m");
+        } else if (n == m) {
+            System.out.println("n equals m");
+        } else {
+            System.out.println("conditions not met");
+        }
+// conditions not met
+
+        /*Задача понять что появится в консоли после каждого блока действий. Что не понятно, спрашиваем в скайпе.
+        Для того что бы увидеть правильный ответ — необходимо выделить мышью пространство после //*/
+
+        int a = 20;
+        int b = a + 5;
+        String s = "Test String";
+        System.out.println(a);
+// 20
+        System.out.println(a + b);
+// 45
+        System.out.println(s + a);
+// Test String20
+        System.out.println(a + s);
+// 20Test String
+        System.out.println(a == b);
+// false
+        System.out.println(a < b);
+// true
+        System.out.println(true && false); // && means AND
+// false
+        System.out.println(a == 20.0);
+// true
+        System.out.println(true || false); // || means OR
+// true
+        if ("20" != s) { System.out.println("a not eqal s"); }
+// a not eqal s
+        if (s == "20") { System.out.println("equals"); } else { System.out.println("not equals"); }
+// not equals
+        if (a > a) {
+            System.out.println("a greater a");
+        } else if (a < a) {
+            System.out.println("a less a");
+        } else {
+            System.out.println("other");
+        }
+// other
+        if (false) { System.out.println("hi there"); }
+//
+        while (a > 0) {
+            System.out.println("Recycle " + b);
+            a = a - 10; } // Recycle 25 // Recycle 25
+    }
+}
---
+++ src/com/company/MainHome.java
@@ -0,0 +1,51 @@
+package com.company;
+
+/**
+ * Created by Ann on 27.03.2015.
+ */
+public class MainHome {
+    public static void main(String[] args){
+        /*boolean pereryv = true;
+        boolean luchshe = false;
+        if (!luchshe && pereryv) {
+            System.out.println("Перерьіву не бьіть.");
+        } if (!(luchshe && pereryv)) {
+            System.out.println("Перерьіву бьіть.");
+        }*/
+    }
+    /*public static int min(int a, int b, int c, int d)
+    {
+        int m2;
+        m2=min(a, b);
+        if (m2<=c & m2<=d) return m2;
+        if (c<=m2 & c<=d) return c;
+        if (d<=c & d<=m2) return d;
+        return m2;
+        //Напишите тут ваш код
+    }
+    public static int min(int a, int b)
+    {
+        int m1;
+        if (a<b) m1=a;
+        else m1=b;
+        return m1;
+        //Напишите тут ваш код
+    }*/
+    public static int min(int a, int b, int c, int d) {
+        int m;
+        m = min(a,b);
+        int f;
+        f = min(c,d);
+        int r;
+        if (m<f) r = m;
+        else r = f;
+        return r;
+    }
+    public static int min(int a, int b)
+    {
+        int r2;
+        if (a<b) r2=a;
+        else r2=b;
+        return r2;
+    }
+}
