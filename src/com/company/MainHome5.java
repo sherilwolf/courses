1c9da9fb295ee75635cfb4c160905efcc8504b30
Ann <sherilwolf@gmail.com>
2015-04-02 20:41:38 +0300

    home add

--- src/com/company/MainHome5.java
+++ src/com/company/MainHome5.java
@@ -5,6 +5,48 @@ package com.company;
  */
 public class MainHome5 {
     public static void main(String[] args) {
+        Stakan s = new Stakan("Super");
+        Stakan k = new Stakan("Bigger");
+        System.out.println("s: " + s.imia);
+        System.out.println("k: " + k.imia);
+        System.out.println("Stakan: " + Stakan.material);
+        System.out.println("Stakan: " + Stakan.vysota);
+        System.out.println("s: " + s.procentZapolneniia());
+// s: Super
+// k: Bigger
+// Stakan: Bumaga
+// Stakan: 10
+// s: 0
+        s.nalit(30);
+        System.out.println("s: " + s.procentZapolneniia());
+// s: 30
+        int v = s.vylit();
+        System.out.println("v: " + v);
+        System.out.println("k: " + k.procentZapolneniia());
+// v: 30
+// k: 0
+        k.nalit(v);
+        System.out.println("k: " + k.procentZapolneniia());
+// k: 30
+        s.nalit(k.vylit());
+        System.out.println("s: " + s.procentZapolneniia());
+        System.out.println("k: " + k.procentZapolneniia());
+// s: 30
+// k: 0
+        s.nalit(50);
+        System.out.println("s: " + s.procentZapolneniia());
+// s: 80
+        s.nalit(45);
+// Super perepolnen. Lishnee vylilos.
+        System.out.println("s: " + s.procentZapolneniia());
+// s: 100
+        s.imia = "ReSuper";
+        System.out.println("s: " + s.imia());
+        System.out.println("k: " + k.imia());
+// s: ReSuper
+// k: Bigger
+        System.out.println("Obiom Stakana vysotoy 20: " + Stakan.obiom(20));
+        // Obiom Stakana vysotoy 20: 400
 
     }
 }
---
+++ src/com/company/Stakan.java
@@ -0,0 +1,40 @@
+package com.company;
+
+/**
+ * Created by Ann on 02.04.2015.
+ */
+public class Stakan {
+    public String imia;
+    public static String material;
+    public static int vysota;
+    public int procentZapolneniia;
+
+    public Stakan(String i){
+        imia = i;
+        material = "Bumaga";
+        vysota = 10;
+    }
+    public int procentZapolneniia(){
+        return procentZapolneniia;
+    }
+    public void nalit(int nalit){
+        procentZapolneniia = procentZapolneniia + nalit;
+        if (procentZapolneniia > 100){
+            procentZapolneniia = 100;
+            System.out.println("Super perepolnen. Lishnee vylilos.");
+        }
+    }
+    public int vylit(){
+        int t = procentZapolneniia;
+        procentZapolneniia = 0;
+        return t;
+    }
+    public String imia(){
+        return imia;
+    }
+    public static int obiom(int obiom){
+
+        obiom = vysota * 20;
+        return obiom;
+    }
+}
